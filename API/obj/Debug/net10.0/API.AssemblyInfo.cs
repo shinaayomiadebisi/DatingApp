ad5d38f3d54f59b3edbd9336627a7921cbc70a5b
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-22 13:02:01 -0800

    update changes

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7117156659759f2ec9a4021f23bfc68cf21d1624")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,39 +1,44 @@
-import { isPlatformBrowser, NgClass } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgClass],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   protected readonly title = signal('The Dating App');
+  users: any[] = [];
 
-  users: any;
+  private platformId = inject(PLATFORM_ID);
+
+  isBrowser = isPlatformBrowser(this.platformId);
+  // isBrowser: boolean;
 
   constructor(
     private http: HttpClient,
-    @Inject(PLATFORM_ID) private platformId: Object, // Client side configuration added here
-  ) {}
+    // @Inject(PLATFORM_ID) private platformId: Object, // Client side configuration added here
+  ) {
+    // this.isBrowser = isPlatformBrowser(this.platformId);
+  }
   ngOnInit() {
-    console.log('Browser?', isPlatformBrowser(this.platformId));
-
     // Client side config condition
-    if (isPlatformBrowser(this.platformId)) {
+    if (this.isBrowser) {
       this.getUsers();
     }
   }
 
   getUsers() {
     this.http.get('http://localhost:5000/api/users').subscribe({
-      next: (response) => {
+      next: (response: any) => {
+        console.log('response', response);
         this.users = response;
       },
       error: (error) => {
-        console.log(error);
+        console.error(error);
       },
     });
   }
