e1e27896715e851d37da86b507a1ebfb0c40f782
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-30 04:40:39 -0800

    user controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,7 @@ namespace API.Controllers
     }
 
     [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
       var users = await _context.Users.ToListAsync();
@@ -23,6 +25,7 @@ namespace API.Controllers
       return users;
     }
 
+    [Authorize]
     // api/users/3
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
