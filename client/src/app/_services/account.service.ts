fbbee9dab039393275a46e6bc3f55aff41c63f99
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-08-26 06:19:26 -0800

    login service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,13 @@ export class AccountService {
   constructor(private http: HttpClient) {}
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model).pipe(map());
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+        }
+      }),
+    );
   }
 }
