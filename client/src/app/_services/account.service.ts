5dcc674a7fb03a102d8af1a60b8baa8f2289982d
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-08-27 01:21:57 -0800

    account service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,23 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+import { ReplaySubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   baseUrl = 'http://localhost:5000/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
-      map((response: any) => {
+      map((response: Partial<User>) => {
         const user = response;
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
         }
       }),
     );
+  }
+
+  // setCurrentUser(){}
+
+  logout() {
+    localStorage.removeItem('user');
   }
 }
