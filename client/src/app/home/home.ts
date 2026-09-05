2806e02cdafa95db567f58d861f738e35c7b913d
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-09-05 00:18:06 -0800

    home ts

--- client/src/app/home/home.ts
+++ client/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  registerMode = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+}
