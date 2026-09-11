e39d63ff487dce43f7868098971fee0e468b9f75
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-09-11 06:12:57 -0800

    home ts update

--- client/src/app/home/home.ts
+++ client/src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Register } from '../register/register';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,17 @@ import { Register } from '../register/register';
 })
 export class Home implements OnInit {
   registerMode = false;
+  users: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5000/api/users').subscribe((users) => (this.users = users));
   }
 }
