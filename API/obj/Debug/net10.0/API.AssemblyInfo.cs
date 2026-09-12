c3f238c5a26d5bef68d0a6fc93bf141fae651940
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-09-12 00:16:28 -0800

    update

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a2c0ade63355079e662b27589f662be331155e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8000600442bd5f07c055087226815d3d28832f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
